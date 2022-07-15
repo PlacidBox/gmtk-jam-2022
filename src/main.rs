dc5597df7addbec8b25091c3edf525080c6ca557
Katherine Broadbent <kategory.bee@gmail.com>
2022-07-15 11:42:25 +1000

    Use ogg, muck around with viewport

--- src/main.rs
+++ src/main.rs
@@ -9,26 +9,31 @@ async fn main() {
         next_frame().await;
     }
 
-    let s = macroquad::audio::load_sound("examples_sound.wav")
+    // ogg exported from audacity seems to work well.
+    let s = macroquad::audio::load_sound("examples_sound.ogg")
         .await
         .unwrap();
 
     macroquad::audio::play_sound_once(s);
 
     loop {
-        let _x = Camera2D::from_display_rect(Rect {
+        let mut _x = Camera2D::from_display_rect(Rect {
             x: 0.,
             y: 0.,
             w: 100.,
             h: 100.,
         });
+        // so set viewport to some set ratio, just to make a simple 2 game a bit easier?
+        // this crops. an alternative is to let any aspect ratio, and not crop the sides, but
+        // that may be a bit more complicated. hmmm. depends on the game?
+        //
+        // this is measured from the bottom left of the screen, as x,y,width,height
+        _x.viewport = Some((500, 0, 800, 600));
         macroquad::camera::set_camera(&_x);
 
         clear_background(RED);
 
         draw_line(0.0, 0.0, 100.0, 100.0, 15.0, BLUE);
-        draw_rectangle(screen_width() / 2.0 - 60.0, 100.0, 120.0, 60.0, GREEN);
-        draw_circle(screen_width() - 30.0, screen_height() - 30.0, 15.0, YELLOW);
         draw_text("Hello, world!", 20.0, 20.0, 20.0, DARKGRAY);
 
         next_frame().await
